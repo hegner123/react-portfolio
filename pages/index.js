124c289ef8de42570bf9ac0cbbda70f8a1cbe149
Michael Hegner <hegner123@gmail.com>
2021-04-08 09:07:38 -0400

    added column and row css, fixed layout issues

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,37 @@
+import React from 'react'
 import Head from 'next/head'
 
 
 export default function Home() {
   return (
+    <div>
+    <Head>
+    <title>Michael Henger</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
     <div className="container">
-      <Head>
-        <title>Michael Henger</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> 
-
-      <main className="container">
-
-      <nav>
-        <div className="logo">
-          <h3>Michael Hegner</h3>
+      <nav className="row">
+        <div className="col-12">
+          <div className="logo">
+             <h3>Michael Hegner</h3>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-        <div className="row">
-          <div className="col-6">
+
             <div className="row">
+              <div className="col-6">
+                <div className="row">
               <div className="col-6">
                 </div>
                 <div className="col-6">
                   <h1>Michael Hegner</h1>
                 </div>
-              </div>
-            </div>
+                </div>
+                </div>
+                
+              
+            
 
             <div className="col-6">
               <div className="row">
@@ -41,10 +46,11 @@ export default function Home() {
                   </div>
                   </div>
                 </div>
-            </div>
+                </div>
+                </div>
+                </div>
             </div>
           </div>
-      </main>
-    </div>
+    
   )
 }
