87b76d1ae3d499043e74aca31964759e2d1a0850
Michael Hegner <hegner123@gmail.com>
2021-04-26 14:41:16 -0400

    trouble shooting

---
+++ Build/static/webpack/pages/index.0f6607953834a0767af4.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./public/assets/projects/projects.json":
+/*!**********************************************!*\
+  !*** ./public/assets/projects/projects.json ***!
+  \**********************************************/
+/*! exports provided: project1, project2, project3, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"project1\\\":{\\\"projectName\\\":\\\"Pro-project Studio\\\",\\\"gitUrl\\\":\\\"https://github.com/hegner123/pro-project-studio\\\",\\\"projectUrl\\\":\\\"https://github.com/hegner123/pro-project-studio\\\"},\\\"project2\\\":{\\\"projectName\\\":\\\"Multiplay Rock, Paper, Scissors\\\",\\\"gitUrl\\\":\\\"https://github.com/hegner123/rps\\\",\\\"projectUrl\\\":\\\"https://hegner123.github.io/rps/\\\"},\\\"project3\\\":{\\\"projectName\\\":\\\"Star Wars Battle\\\",\\\"gitUrl\\\":\\\"https://github.com/hegner123/starwarsbattle\\\",\\\"projectUrl\\\":\\\"https://hegner123.github.io/starwarsbattle/\\\"}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3B1YmxpYy9hc3NldHMvcHJvamVjdHMvcHJvamVjdHMuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./public/assets/projects/projects.json\n");
+
+/***/ })
+
+})
