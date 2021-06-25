c9fb765f1b6e6f5a9e4b178b2f7f94d3230bac49
Michael Hegner <hegner123@gmail.com>
2021-06-25 11:26:42 -0400

    redesign init

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { css } from 'styled-components';
-
 import {About} from '../components/about';
 import { Projects } from '../components/projects';
 import { Main, NavBar, NavList, NavItem } from './style';
 
 
-export function HomePage(props){
+export function HomePage(){
 
         return (
          <div className="homepage">
@@ -23,35 +21,15 @@ export function HomePage(props){
                             <NavItem>
                                 <a href="https://www.linkedin.com/in/michaelhegner/" target="_none" css={"color:var(--text-color);text-decoration:none;"}>LinkedIn</a>
                             </NavItem>
-                            <NavItem>
-                                <a href="public/Resume.pdf" target="_none" css={"color:var(--text-color);text-decoration:none;"}>Resume</a>
-                            </NavItem>
-                            <NavItem>
-                                <a href="mailto:hegner123@gmail.com" css={"color:var(--text-color);text-decoration:none;"}>Email</a>
-                            </NavItem>
                         </NavList>
                     </NavBar>
                 </nav>
             </header>
             <Main css={"display:flex;width:100%"}>
+                
                 <About />
                 <Projects/>
             </Main>
         </div>
         )
     }
-
-
-// function mapState(state) {
-//     const { users, authentication } = state;
-//     const { user } = authentication;
-//     return { user, users };
-// }
-
-// const actionCreators = {
-//     getUsers: userActions.getAll,
-//     deleteUser: userActions.delete
-// }
-
-// const connectedHomePage = connect(mapState, actionCreators)(HomePage);
-// export { connectedHomePage as HomePage };
